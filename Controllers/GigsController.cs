aeeb44311a0101f076a49c6d014afe68798ec437
Abdelrahman ghrayeb <abedghrayeb@gmail.com>
2019-06-15 04:57:32 +0300

    create gig form

---
+++ Controllers/GigsController.cs
@@ -0,0 +1,26 @@
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public GigsController()
+        {
+            context = new ApplicationDbContext();
+        }
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            GigFormViewModel viewModel = new GigFormViewModel
+            {
+                Genres = context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
